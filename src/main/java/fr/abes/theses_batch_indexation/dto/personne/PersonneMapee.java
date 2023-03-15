9bd437f527fa9f8e969682fb50f76a193848e86d
Fabien Rétif <fabien.retif@zoho.com>
2023-03-15 18:06:46 +0100

    Refactor + Ajout du parsing des rôles pour les personnes

--- src/main/java/fr/abes/theses_batch_indexation/dto/personne/PersonneMapee.java
+++ src/main/java/fr/abes/theses_batch_indexation/dto/personne/PersonneMapee.java
@@ -8,30 +8,52 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * L'objet PersonneMappe correspond à un objet transitoire entre le format TEF (XML) et le format JSON attendu par Elastic Search.
+ * L'objectif est de faire correspondre les données du format TEF (préparser par la libraire Mets) avec les données au format Elastic Search.
+ * Les transformations sont réalisées directement dans le constructeur de l'objet.
+ */
 @Slf4j
 @Getter
 public class PersonneMapee {
 
-    //Todo: Ajouter les champs à indexer dans personneModel ou thesesDOT
+    /**
+     * Liste des personnes (au format Elastic Search) figurant dans le fichier TEF
+     */
     private List<PersonneModelES> personnes = new ArrayList<>();
 
     private TheseModelESPartiel theseModelESPartiel = new TheseModelESPartiel();
 
+    /**
+     * Identifiant de la thèse
+     */
     private String nnt;
 
     private final String AUTEUR = "auteur";
-    private final String DIRECTEUR = "directeur";
+    private final String DIRECTEUR = "directeur de thèse";
+    private final String RAPPORTEUR = "rapporteur";
+    private final String PRESIDENT = "président du jury";
+    private final String MEMBRE_DU_JURY = "membre du jury";
 
     /*Todo: à partir de Mets extraire toutes les infos à indexer
        (s'inpirer de TheseMapee vu que la plupart des champs sont déjà extrait)*/
+
+    /**
+     * CTOR
+     * Cette fonction permet de remplir la liste des personnes à partir du TEF.
+     * En cas d'erreur de lecture, les erreurs sont affichées dans les logs.
+     * @param mets
+     */
     public PersonneMapee(Mets mets) {
 
         DmdSec dmdSec = mets.getDmdSec().get(1);
         AmdSec amdSec = mets.getAmdSec().get(0);
 
         TechMD techMD = null;
-        // nnt
 
+        /************************************
+         * Parsing du NNT
+         * ***********************************/
         try {
 
             techMD = amdSec.getTechMD().stream().filter(d -> d.getMdWrap().getXmlData().getThesisAdmin() != null).findFirst().orElse(null);
@@ -47,68 +69,75 @@ public class PersonneMapee {
             log.error("PB pour nnt " + e.toString());
         }
 
+        /************************************
+         * Parsing du titre principal de la thèse
+         * ***********************************/
         try {
             theseModelESPartiel.setTitre(dmdSec.getMdWrap().getXmlData().getThesisRecord().getTitle().getContent());
         } catch (NullPointerException e) {
             log.error("PB pour titrePrincipal de " + nnt + e.getMessage());
         }
 
+        /************************************
+         * Parsing des auteurs de la thèse
+         * ***********************************/
+        log.info("traitement des rapporteurs");
         try {
-            List<PersonneModelES> auteurs = new ArrayList<>();
-
-            List<Auteur> auteursDepuisTef = techMD.getMdWrap().getXmlData().getThesisAdmin()
-                    .getAuteur();
-            Iterator<Auteur> auteurIterator = auteursDepuisTef.iterator();
-            while (auteurIterator.hasNext()) {
-                Auteur a = auteurIterator.next();
-
-                PersonneModelES adto = new PersonneModelES(
-                        a.getAutoriteExterne() != null ? a.getAutoriteExterne().getValue() : null,
-                        a.getNom(),
-                        a.getPrenom()
-                );
-
-                TheseModelES e = new TheseModelES(theseModelESPartiel, AUTEUR);
-
-                adto.getTheses().add(e);
-                auteurs.add(adto);
-            }
-
-            personnes.addAll(auteurs);
+            personnes.addAll(parseAuteurs(techMD.getMdWrap().getXmlData().getThesisAdmin()
+                    .getAuteur()));
 
         } catch (NullPointerException e) {
             log.error("PB pour auteurs de " + nnt + "," + e.getMessage());
         }
 
-        // directeurs
+        /************************************
+         * Parsing des directeurs de la thèse
+         * ***********************************/
         log.info("traitement de directeurs");
         try {
-
-            List<PersonneModelES> directeurs = new ArrayList<>();
-
-            List<DirecteurThese> directeursDepuisTef = techMD.getMdWrap().getXmlData().getThesisAdmin()
-                    .getDirecteurThese();
-            Iterator<DirecteurThese> directeurTheseIterator = directeursDepuisTef.iterator();
-            while (directeurTheseIterator.hasNext()) {
-                DirecteurThese dt = directeurTheseIterator.next();
-
-                PersonneModelES personneModelES = new PersonneModelES(
-                        dt.getAutoriteExterne() != null ? dt.getAutoriteExterne().getValue() : null,
-                        dt.getNom(),
-                        dt.getPrenom()
-                );
-
-                TheseModelES e = new TheseModelES(theseModelESPartiel, DIRECTEUR);
-
-                personneModelES.getTheses().add(e);
-                directeurs.add(personneModelES);
-            }
-
-            personnes.addAll(directeurs);
+            personnes.addAll(parseDirecteurs(techMD.getMdWrap().getXmlData().getThesisAdmin()
+                    .getDirecteurThese()));
 
         } catch (NullPointerException e) {
             log.error("PB pour directeurs de " + nnt + "," + e.getMessage());
         }
+
+        /************************************
+         * Parsing des rapporteurs de la thèse
+         * ***********************************/
+        log.info("traitement des rapporteurs");
+        try {
+            personnes.addAll(parseRapporteurs(techMD.getMdWrap().getXmlData().getThesisAdmin()
+                    .getDirecteurThese()));
+
+        } catch (NullPointerException e) {
+            log.error("PB pour les rapporteurs de " + nnt + "," + e.getMessage());
+        }
+
+        /************************************
+         * Parsing des présidents du jury de la thèse
+         * ***********************************/
+        log.info("traitement des présidents du jury de la thèse");
+        try {
+            personnes.addAll(parsePresident(techMD.getMdWrap().getXmlData().getThesisAdmin()
+                    .getDirecteurThese()));
+
+        } catch (NullPointerException e) {
+            log.error("PB pour les présidents du jury de " + nnt + "," + e.getMessage());
+        }
+
+        /************************************
+         * Parsing des membres du jury de la thèse
+         * ***********************************/
+        log.info("traitement des membres du jury de la thèse");
+        try {
+            personnes.addAll(parseMembreJury(techMD.getMdWrap().getXmlData().getThesisAdmin()
+                    .getDirecteurThese()));
+
+        } catch (NullPointerException e) {
+            log.error("PB pour les membres du jury de " + nnt + "," + e.getMessage());
+        }
+
     }
 
     private boolean isNnt(String identifier) {
@@ -116,4 +145,125 @@ public class PersonneMapee {
             return true;
         return false;
     }
+
+    private PersonneModelES buildPersonne(String id, String nom, String prenom, String role, TheseModelESPartiel theses) {
+        // Construction d'un objet Personne
+        PersonneModelES item = new PersonneModelES(
+                id,
+                nom,
+                prenom
+        );
+        TheseModelES e = new TheseModelES(theseModelESPartiel, role);
+        item.getTheses().add(e);
+        item.getRoles().add(role);
+
+        return item;
+    }
+
+    private List<PersonneModelES> parseAuteurs(List<Auteur> auteursDepuisTef) throws NullPointerException {
+        List<PersonneModelES> candidates = new ArrayList<>();
+
+        Iterator<Auteur> auteurIterator = auteursDepuisTef.iterator();
+        while (auteurIterator.hasNext()) {
+            Auteur item = auteurIterator.next();
+
+            // Construction d'un objet Personne
+            PersonneModelES adto = buildPersonne(
+                    item.getAutoriteExterne() != null ? item.getAutoriteExterne().getValue() : null,
+                    item.getNom(),
+                    item.getPrenom(),
+                    AUTEUR,
+                    theseModelESPartiel
+            );
+            candidates.add(adto);
+        }
+
+        return candidates;
+    }
+
+    private List<PersonneModelES> parseDirecteurs(List<DirecteurThese> directeursDepuisTef) throws NullPointerException {
+        List<PersonneModelES> candidates = new ArrayList<>();
+
+        Iterator<DirecteurThese> directeurTheseIterator = directeursDepuisTef.iterator();
+        while (directeurTheseIterator.hasNext()) {
+            DirecteurThese item = directeurTheseIterator.next();
+
+            // Construction d'un objet Personne
+            PersonneModelES adto = buildPersonne(
+                    item.getAutoriteExterne() != null ? item.getAutoriteExterne().getValue() : null,
+                    item.getNom(),
+                    item.getPrenom(),
+                    DIRECTEUR,
+                    theseModelESPartiel
+            );
+            candidates.add(adto);
+        }
+
+        return candidates;
+    }
+
+    private List<PersonneModelES> parseRapporteurs(List<DirecteurThese> directeursDepuisTef) throws NullPointerException {
+        List<PersonneModelES> candidates = new ArrayList<>();
+
+        Iterator<DirecteurThese> directeurTheseIterator = directeursDepuisTef.iterator();
+        while (directeurTheseIterator.hasNext()) {
+            DirecteurThese item = directeurTheseIterator.next();
+
+            // Construction d'un objet Personne
+            PersonneModelES adto = buildPersonne(
+                    item.getAutoriteExterne() != null ? item.getAutoriteExterne().getValue() : null,
+                    item.getNom(),
+                    item.getPrenom(),
+                    RAPPORTEUR,
+                    theseModelESPartiel
+            );
+            candidates.add(adto);
+        }
+
+        return candidates;
+    }
+
+    private List<PersonneModelES> parsePresident(List<DirecteurThese> directeursDepuisTef) throws NullPointerException {
+        List<PersonneModelES> candidates = new ArrayList<>();
+
+        Iterator<DirecteurThese> directeurTheseIterator = directeursDepuisTef.iterator();
+        while (directeurTheseIterator.hasNext()) {
+            DirecteurThese item = directeurTheseIterator.next();
+
+            // Construction d'un objet Personne
+            PersonneModelES adto = buildPersonne(
+                    item.getAutoriteExterne() != null ? item.getAutoriteExterne().getValue() : null,
+                    item.getNom(),
+                    item.getPrenom(),
+                    PRESIDENT,
+                    theseModelESPartiel
+            );
+            candidates.add(adto);
+        }
+
+        return candidates;
+    }
+
+    private List<PersonneModelES> parseMembreJury(List<DirecteurThese> directeursDepuisTef) throws NullPointerException {
+        List<PersonneModelES> candidates = new ArrayList<>();
+
+        Iterator<DirecteurThese> directeurTheseIterator = directeursDepuisTef.iterator();
+        while (directeurTheseIterator.hasNext()) {
+            DirecteurThese item = directeurTheseIterator.next();
+
+            // Construction d'un objet Personne
+            PersonneModelES adto = buildPersonne(
+                    item.getAutoriteExterne() != null ? item.getAutoriteExterne().getValue() : null,
+                    item.getNom(),
+                    item.getPrenom(),
+                    MEMBRE_DU_JURY,
+                    theseModelESPartiel
+            );
+            candidates.add(adto);
+        }
+
+        return candidates;
+    }
+
+
 }
--- src/main/java/fr/abes/theses_batch_indexation/dto/personne/PersonneModelES.java
+++ src/main/java/fr/abes/theses_batch_indexation/dto/personne/PersonneModelES.java
@@ -1,23 +1,39 @@
 package fr.abes.theses_batch_indexation.dto.personne;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.extern.jackson.Jacksonized;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Représente une personne au format de l'index Elastic Search.
+ * Cet objet sera transformé en JSON au moment de l'appel API pour l'indexation.
+ */
 @Getter
 @Setter
 @NoArgsConstructor
 public class PersonneModelES {
+
     private String ppn;
     private boolean has_idref;
     private String nom;
     private String prenom;
-    private List<TheseModelES> theses = new ArrayList<>();
-    private List<String> roles = new ArrayList<>();
+    private List<String> nom_complet = new ArrayList<>();
+    private List<SuggestionES> suggestion = new ArrayList<>();
 
+    private List<TheseModelES> theses = new ArrayList<>();
+
+    /*
+    TODO : Pourquoi rajouter un champs roles à ce niveau
+     alors qu'il peut être retrouver depuis la liste des thèses ?
+     */
+    private List<String> roles = new ArrayList<>();
 
     public PersonneModelES(String ppn, String nom, String prenom) {
         if (ppn != null && !ppn.equals("")) {
@@ -28,5 +44,11 @@ public class PersonneModelES {
         }
         this.nom = nom;
         this.prenom = prenom;
+
+        nom_complet.add(String.format("%1$s %2$s",prenom,nom));
+        nom_complet.add(String.format("%1$s %2$s",nom,prenom));
+
+        suggestion.add(SuggestionES.builder().input(String.format("%1$s %2$s",prenom,nom)).weight(10).build());
+        suggestion.add(SuggestionES.builder().input(String.format("%1$s %2$s",nom,prenom)).weight(10).build());
     }
 }
