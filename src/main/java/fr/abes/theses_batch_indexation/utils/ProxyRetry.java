b405d98ba525600b97ebe1057e5130a493139bed
gibert <gibert@abes.fr>
2023-06-16 10:51:32 +0200

    [feat] ajout retry pour indexation des thèses dans ES

---
+++ src/main/java/fr/abes/theses_batch_indexation/utils/ProxyRetry.java
@@ -0,0 +1,21 @@
+package fr.abes.theses_batch_indexation.utils;
+
+import co.elastic.clients.elasticsearch.core.BulkRequest;
+import co.elastic.clients.elasticsearch.core.BulkResponse;
+import fr.abes.theses_batch_indexation.configuration.ElasticClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class ProxyRetry {
+
+    @Retryable(maxAttempts = 4, backoff = @Backoff(delay = 1000, multiplier = 2) )
+    public BulkResponse indexerDansES(BulkRequest.Builder br) throws IOException {
+        return ElasticClient.getElasticsearchClient().bulk(br.build());
+    }
+}
