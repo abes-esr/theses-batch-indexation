3546b21ae5ca42e67e4d2c5ad99ba59e0329f378
gibert <gibert@abes.fr>
2023-12-06 16:05:23 +0100

    ajout dockerisation du batch

--- docker/batch/theses-indexation.sh
+++ docker/batch/theses-indexation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-exec java -jar /scripts/theses-indexation.jar \
-          -Duser.timezone=Europe/Paris \
-          -Duser.language=fr
+LANG=fr_FR.UTF-8
+if [[ $(pgrep -cf "theses-indexation.jar --spring.batch.job.names=indexationThesesDansES") < 1 ]];
+then
+    java -jar /scripts/theses-indexation.jar --spring.batch.job.names=indexationThesesDansES
+fi
