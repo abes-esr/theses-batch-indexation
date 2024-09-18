8f0370aefb1e8bbff65203ef5baaec46e14d52ea
le-maire <le-maire@abes.fr>
2024-09-18 11:43:13 +0200

    fix: rollback de la maj de jaxb, car on ne peut pas générer les classes tef

--- src/main/java/fr/abes/theses_batch_indexation/model/tef/TelephoneDoctorant.java
+++ src/main/java/fr/abes/theses_batch_indexation/model/tef/TelephoneDoctorant.java
@@ -8,10 +8,10 @@
 
 package fr.abes.theses_batch_indexation.model.tef;
 
-import jakarta.xml.bind.annotation.XmlAccessType;
-import jakarta.xml.bind.annotation.XmlAccessorType;
-import jakarta.xml.bind.annotation.XmlRootElement;
-import jakarta.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 
 
 /**
